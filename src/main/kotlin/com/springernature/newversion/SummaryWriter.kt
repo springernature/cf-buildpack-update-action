06d49859755679fc8f88730d2840b233b62def08
Elmar Fasel <Elmar.Fasel@springernature.com>
2022-05-16 10:11:35 +0200

    Write only if there is something to write

--- src/main/kotlin/com/springernature/newversion/SummaryWriter.kt
+++ src/main/kotlin/com/springernature/newversion/SummaryWriter.kt
@@ -30,11 +30,19 @@ class SummaryWriter(val file: File) {
     }
 
     private fun writeSuccessfulUpdates(updates: List<BuildpackUpdate>) {
+        if (updates.isEmpty()) {
+            return
+        }
+
         file.appendText("\n## success\n\n")
         updates.forEach { file.appendText("* currentBuildpack ${it.currentBuildpack} ${if (it.hasUpdate()) "has an update to " + it.latestUpdate else "has no update"}\n") }
     }
 
     private fun writeFailures(errors: Map<BuildpackUpdate, Exception>) {
+        if (errors.isEmpty()) {
+            return
+        }
+
         file.appendText(
             "\n## failures\n" +
                     "\n"
