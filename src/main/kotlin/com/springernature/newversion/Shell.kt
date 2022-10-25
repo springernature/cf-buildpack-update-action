bb047c2f01a7aa505bb9dd88ea04f3a796626bdc
Dennis Hoersch <dennis.hoersch@springernature.com>
2022-10-25 15:58:19 +0200

    Logs the process output while a command is being executed and returns the output afterwards as before

--- src/main/kotlin/com/springernature/newversion/Shell.kt
+++ src/main/kotlin/com/springernature/newversion/Shell.kt
@@ -1,5 +1,8 @@
 package com.springernature.newversion
 
+import com.lordcodes.turtle.ProcessCallbacks
+import com.lordcodes.turtle.ProcessOutput
+import com.lordcodes.turtle.ShellRunException
 import com.lordcodes.turtle.shellRun
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -24,12 +27,57 @@ class TurtleShell : Shell {
 class TurtleScript(private val workingDirectory: File?) : Script {
     override fun command(command: String, arguments: List<String>): String {
         return shellRun(workingDirectory) {
-            LOG.debug("Shell: {} {}", command, arguments.joinToString(" ") { """"$it"""" })
-            command(command, arguments)
+            val commandDescription = "$command ${arguments.joinToString(" ") { """"$it"""" }}"
+            LOG.debug("Shell: {}", commandDescription)
+            val callback = LogAndCollectOutputs(commandDescription)
+            val processOut = commandStreaming(command, arguments, callback)
+            processOut.retrieveOutputOrThrow(callback.collectedStdOut(), callback.collectedStdErr())
         }
+    }
+
+    private fun ProcessOutput.retrieveOutputOrThrow(
+        previouslyCollectedStdOut: String,
+        previouslyCollectedStdErr: String
+    ): String {
+        val outputText = previouslyCollectedStdOut.trim()
+        if (exitCode != 0) {
+            val errorText = previouslyCollectedStdErr.trim()
+            if (errorText.isNotEmpty()) {
+                throw ShellRunException(exitCode, errorText)
+            }
+        }
+        return outputText
     }
 
     companion object {
         private val LOG: Logger = LoggerFactory.getLogger(TurtleScript::class.java)
+    }
+}
+
+
+private class LogAndCollectOutputs(private val commandDescription: String) : ProcessCallbacks {
+    private val stdOut = mutableListOf<String>()
+    private val stdErr = mutableListOf<String>()
+
+    override fun onProcessStart(process: Process) {
+        process.inputStream.bufferedReader().useLines {
+            it.forEach { line ->
+                LOG.info("Shell ({}): {}", commandDescription, line)
+                stdOut += line
+            }
+        }
+        process.errorStream.bufferedReader().useLines {
+            it.forEach { line ->
+                LOG.error("Shell ({}): {}", commandDescription, line)
+                stdErr += line
+            }
+        }
+    }
+
+    fun collectedStdOut(): String = stdOut.joinToString("\n")
+    fun collectedStdErr(): String = stdErr.joinToString("\n")
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(LogAndCollectOutputs::class.java)
     }
 }
---
+++ src/test/kotlin/com/springernature/newversion/turtleScriptTestScript.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+for i in $(seq 1 2); do
+   echo "$i"
+   sleep 1
+done
